978f6b349671f964fa480ff561bf8d3bb441dfb8
anaparaguassu <analuparaguassu01@gmail.com>
2026-04-23 18:39:24 -0300

    :sparkles: feat: cria roteamento CLI para extensões .asm, .pre e .obj

--- montador.cpp
+++ montador.cpp
@@ -34,3 +34,50 @@ map<string, Instruction> instructionTable = {
     {"OUTPUT", {13, 2}},
     {"STOP", {14, 1}}
 };
+
+void runPreprocessor(string filename);
+void runAssembler(string filename);
+void runSimulator(string filename);
+
+int main(int argc, char* argv[]) {
+    // verifica se há o nome do arquivo
+    if(argc != 2){
+        cout << "Erro! Execute: ./montador <arquivo>\n";
+        return 1;
+    }
+
+    string fileArg = argv[1];
+
+    // pega posição para verificar extensão
+    size_t dotPos = fileArg.find_last_of('.');
+    if(dotPos == string::npos){
+        cout << "Erro: Arquivo sem extensão.\n";
+        return 1;
+    }
+
+    string extension = fileArg.substr(dotPos);
+    string baseFilename = fileArg.substr(0, dotPos);
+
+    // roteamento dos 3 modos de execução
+    if(extension == ".asm"){
+        cout << "Modo: Pre-processador\n";
+        // deve gerar o arquivo .pre
+        runPreprocessor(baseFilename);
+    }
+    else if (extension == ".pre"){
+        cout << "Modo: Montador\n";
+        // deve gerar os arquivos .obj e .pen
+        runAssembler(baseFilename);
+    }
+    else if(extension == ".obj"){
+        cout << "Modo: Simulador\n";
+        // deve simular a execução lendo INPUT do teclado e OUTPUT pro monitor 
+        runSimulator(baseFilename);
+    }
+    else{
+        cout << "Erro: Extensão nao suportada ()" << extension << ").\n";
+        return 1;
+    }
+    
+    return 0;
+}
